c5dff1cefc349f5af0f13b62b9fa1f466c6e6e6f
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2025-02-24 00:00:17 +0500

    fix errors again

--- app/answers/[id]/page.tsx
+++ app/answers/[id]/page.tsx
@@ -5,13 +5,14 @@ import { notFound } from "next/navigation";
 import EditableAnswers from "@/components/answer-editor";
 
 interface AnswersPageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default async function AnswersPage({ params }: AnswersPageProps) {
+  const resolvedParams = await params;
   const session = await auth.api.getSession({ headers: await headers() });
   const userId = session?.user?.id;
-  const formId = params.id;
+  const formId = resolvedParams.id;
 
   if (!userId) {
     return <p className="text-center mt-10">You should be authorized</p>;
